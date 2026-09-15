38f9f5e7816f1342fd0f99fa092c6be935fd76c6
Liu Shiyu <104006363+liush2yuxjtu@users.noreply.github.com>
2026-09-15 10:56:21 +0800

    Wire shared 3D tokens into WinBrain runtime

---
+++ source/tokens/apply.js
@@ -0,0 +1,79 @@
+import * as THREE from "three";
+import {
+  renderer,
+  scene,
+  camera,
+  target,
+  view,
+  updateCamera,
+  silver,
+  white,
+  black,
+  glass,
+  tubeGlass,
+  levels,
+} from "../core.js";
+import { TOKENS, hex } from "./tokens.js";
+
+function setPhysical(material, values) {
+  for (const [key, value] of Object.entries(values)) {
+    if (key in material) material[key] = value;
+  }
+  material.needsUpdate = true;
+}
+
+function setDirectional(light, token) {
+  light.color.setHex(hex(token.color));
+  light.intensity = token.intensity;
+  light.position.set(...token.position);
+}
+
+export function applyDesignTokens() {
+  renderer.toneMappingExposure = TOKENS.rendering.toneMappingExposure;
+  scene.environmentIntensity = TOKENS.rendering.environmentIntensity;
+
+  const ortho = TOKENS.camera.ortho;
+  camera.left = ortho.left;
+  camera.right = ortho.right;
+  camera.top = ortho.top;
+  camera.bottom = ortho.bottom;
+  camera.near = ortho.near;
+  camera.far = ortho.far;
+  target.set(...TOKENS.camera.target);
+  view.yaw = TOKENS.camera.yaw;
+  view.pitch = TOKENS.camera.pitch;
+  view.zoom = TOKENS.camera.zoom;
+  updateCamera();
+
+  const hemi = scene.children.find((node) => node.isHemisphereLight);
+  if (hemi) {
+    hemi.color.setHex(hex(TOKENS.lighting.hemisphere.sky));
+    hemi.groundColor.setHex(hex(TOKENS.lighting.hemisphere.ground));
+    hemi.intensity = TOKENS.lighting.hemisphere.intensity;
+  }
+  const directionals = scene.children.filter((node) => node.isDirectionalLight);
+  if (directionals[0]) setDirectional(directionals[0], TOKENS.lighting.key);
+  if (directionals[1]) setDirectional(directionals[1], TOKENS.lighting.rim);
+  if (directionals[2]) setDirectional(directionals[2], TOKENS.lighting.front);
+
+  silver.color.setHex(hex(TOKENS.color.silver));
+  setPhysical(silver, TOKENS.material.silver);
+  white.color.setHex(hex(TOKENS.color.white));
+  setPhysical(white, TOKENS.material.white);
+  black.color.setHex(hex(TOKENS.color.black));
+
+  glass.color.setHex(hex(TOKENS.color.glass));
+  setPhysical(glass, TOKENS.material.glass);
+  glass.side = THREE.DoubleSide;
+  glass.transparent = true;
+  glass.depthWrite = false;
+
+  tubeGlass.color.setHex(hex(TOKENS.color.tubeGlass));
+  setPhysical(tubeGlass, TOKENS.material.tubeGlass);
+  tubeGlass.transparent = true;
+  tubeGlass.depthWrite = false;
+
+  levels[3] = TOKENS.layers.applicationY;
+  levels[2] = TOKENS.layers.intelligenceY;
+  levels[1] = TOKENS.layers.dataY;
+}
