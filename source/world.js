da212568ab8260512aeb84bedd25b7528d51a6b3
Liu Shiyu <104006363+liush2yuxjtu@users.noreply.github.com>
2026-09-15 10:56:30 +0800

    Apply shared design tokens before building 3D world

--- source/world.js
+++ source/world.js
@@ -1,3 +1,4 @@
+import { applyDesignTokens } from "./tokens/apply.js";
 import { createPlatforms } from "./components/platforms.js";
 import { createColumns } from "./components/columns.js";
 import { createActors } from "./components/actors.js";
@@ -7,7 +8,9 @@ import { createCity } from "./components/city.js";
 import { createDataObjects } from "./components/data-objects.js";
 import { createEarth } from "./components/earth.js";
 import { createAtmosphere } from "./components/atmosphere.js";
+
 export function buildWorld() {
+  applyDesignTokens();
   createPlatforms();
   createColumns();
   createActors();
